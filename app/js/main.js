b1fd4b1ae2dd6d94c4f59aaa37be619a7a2477b8
5for3 <lidzarvl@gmail.com>
2022-07-03 19:37:41 +0300

    add features, update fonts

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,8 @@ $(function () {
   $('.slider-top__inner').slick({
     dots: true,
     arrows: false,
+    fade: true,
+    pauseOnDotsHover: true,
     autoplay: true,
     autoplaySpead: 3000
   })
